423c46216e8bef8e728470f25729b137cf0cc890
John Denver <chogathkk@gmail.com>
2022-04-06 15:55:51 +0800

    feat: first commit 🌈

--- src/base/adapter.ts
+++ src/base/adapter.ts
@@ -1,12 +1,12 @@
 import { ClientAdapter, FixedContext } from '@vodyani/core';
 
-import { IoRedis, RedisOptions } from '../common';
+import { BaseRedis, RedisOptions } from '../common';
 
 export class RedisAdapter implements ClientAdapter {
   public instance: any;
 
   constructor(options: RedisOptions) {
-    this.instance = new IoRedis(options);
+    this.instance = new BaseRedis(options);
   }
 
   @FixedContext
--- src/common/declare.ts
+++ src/common/declare.ts
@@ -1,10 +1,5 @@
-import * as IoRedis from 'ioredis';
+import BaseRedis from 'ioredis';
 
-export { IoRedis };
+export { BaseRedis };
 
-export type Redis = IoRedis.Redis;
-export type RedisCluster = IoRedis.Cluster;
-export type RedisOptions = IoRedis.RedisOptions;
-export type RedisClusterNode = IoRedis.ClusterNode;
-export type RedisClusterOptions = IoRedis.ClusterOptions;
-
+export { RedisOptions, Cluster, ClusterNode, ClusterOptions } from 'ioredis';
